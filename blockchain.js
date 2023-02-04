3d85e8d37251def37a4d80bbd041ce4e6be51c00
mathesond2 <mathesond2@gmail.com>
2023-02-04 12:06:29 -0500

    add npm pkg for node types, convert js to ts

--- blockchain.js
+++ blockchain.js
@@ -1,91 +1,77 @@
-const sha256 = require('crypto-js/sha256');
-
-class Block {
-  constructor(txns, prevHash = "") {
-    this.txns = txns;
-    this.prevHash = prevHash;
-    this.hash = this.computeHash();
-  }
-
-  computeHash() {
-    return sha256(this.prevHash + JSON.stringify(this.data)).toString();
-  }
-}
-
-class BlockChain {
-  constructor() {
-    this.blockchain = [this.startGenesisBlock()];
-    this.blockSize = 2;
-    this.state = null;
-  }
-
-  startGenesisBlock() {
-    return new Block({});
-  }
-
-  getLatestBlock() {
-    return this.blockchain[this.blockchain.length - 1];
-  }
-  //TODO: implement TS
-  isValidBlock(newBlock) {
-    const { txns } = newBlock;
-    if (typeof txns !== 'object') { // remove upon implementing TS
-      return false;
+var sha256 = require('crypto-js/sha256');
+var Block = /** @class */ (function () {
+    function Block(txns, prevHash) {
+        if (prevHash === void 0) { prevHash = ''; }
+        this.txns = txns;
+        this.prevHash = prevHash;
+        this.txns = txns;
+        this.prevHash = prevHash;
+        this.hash = this.computeHash();
     }
-
-    const txnKeys = Object.keys(txns);
-    if (txnKeys.length !== this.blockSize) {
-      return false;
+    Block.prototype.computeHash = function () {
+        return sha256(this.prevHash + JSON.stringify(this.txns)).toString();
+    };
+    return Block;
+}());
+var BlockChain = /** @class */ (function () {
+    function BlockChain() {
+        this.blockchain = [this.startGenesisBlock()];
+        this.blockSize = 2;
+        this.state = null;
     }
-
-    const areCorrectKeys = txnKeys.map(key => ['from', 'to'].includes(key) ? true : false);
-    return areCorrectKeys.every(key => key === true);
-  }
-
-  addNewBlock(newBlock) {
-    if (!this.isValidBlock(newBlock)) {
-      return;
-    }
-    newBlock.prevHash = this.getLatestBlock().hash; // Set its previous hash to the correct value
-    newBlock.hash = newBlock.computeHash(); // Recalculate its hash with this new prevHash value
-    this.blockchain.push(newBlock); // Add the block to our chain
-    this.state = newBlock.txns;
-  }
-
-  getBlock(hash) {
-    return this.blockchain.find(block => block.hash === hash);
-  }
-
-  getAccountBalance(acount) {
-    return this.state.find(state => state.acount === acount);
-  }
-
-  isValidChain() { // Check to see that all the hashes are correct and the chain is therefore valid
-    for (let i = 1; i < this.blockchain.length; i++) { // Iterate through, starting after the genesis block
-      const currBlock = this.blockchain[i];
-      const prevBlock = this.blockchain[i -1];
-
-      // Is the hash correctly computed, or was it tampered with?
-      if (currBlock.hash !== currBlock.computeHash()) {
-        return false;
-      }
-
-      // Does it have the correct prevHash value?; ie: What a previous block tampered with?
-      if (currBlock.prevHash !== prevBlock.hash) {
-        return false;
-      }
-    }
-    return true; // If all the blocks are valid, return true
-  }
-}
-
-const a = new Block({from: "Joe", to: "Jane"})
-const b = new Block({from: "Jane", to: "Joe"})
-const c = new Block({from: "Jane", ton: "Joe"})
-
-const chain = new BlockChain();
+    BlockChain.prototype.startGenesisBlock = function () {
+        return new Block({});
+    };
+    BlockChain.prototype.getLatestBlock = function () {
+        return this.blockchain[this.blockchain.length - 1];
+    };
+    BlockChain.prototype.isValidBlock = function (newBlock) {
+        var txns = newBlock.txns;
+        var txnKeys = Object.keys(txns);
+        if (txnKeys.length !== this.blockSize) {
+            return false;
+        }
+        var areCorrectKeys = txnKeys.map(function (key) { return ['from', 'to'].includes(key) ? true : false; });
+        return areCorrectKeys.every(function (key) { return key === true; });
+    };
+    BlockChain.prototype.addNewBlock = function (newBlock) {
+        if (!this.isValidBlock(newBlock)) {
+            return;
+        }
+        newBlock.prevHash = this.getLatestBlock().hash; // Set its previous hash to the correct value
+        newBlock.hash = newBlock.computeHash(); // Recalculate its hash with this new prevHash value
+        this.blockchain.push(newBlock); // Add the block to our chain
+        this.state = newBlock.txns;
+    };
+    BlockChain.prototype.getBlock = function (hash) {
+        return this.blockchain.find(function (block) { return block.hash === hash; });
+    };
+    BlockChain.prototype.getAccountBalance = function (account) {
+        return this.state.find(function (state) { return state.account === account; });
+    };
+    BlockChain.prototype.isValidChain = function () {
+        for (var i = 1; i < this.blockchain.length; i++) { // Iterate through, starting after the genesis block
+            var currBlock = this.blockchain[i];
+            var prevBlock = this.blockchain[i - 1];
+            // Is the hash correctly computed, or was it tampered with?
+            if (currBlock.hash !== currBlock.computeHash()) {
+                return false;
+            }
+            // Does it have the correct prevHash value?; ie: What a previous block tampered with?
+            if (currBlock.prevHash !== prevBlock.hash) {
+                return false;
+            }
+        }
+        return true; // If all the blocks are valid, return true
+    };
+    return BlockChain;
+}());
+var a = new Block({ from: "Joe", to: "Jane" });
+var b = new Block({ from: "Jane", to: "Joe" });
+// const c = new Block({from: "Jane", to: {name: "Joe"}})
+var chain = new BlockChain();
 chain.addNewBlock(a);
 chain.addNewBlock(b);
-chain.addNewBlock(c);
+// chain.addNewBlock(c);
 console.log(chain);
-console.log(`is valid chain: ${chain.isValidChain()}`);
+console.log("is valid chain: ".concat(chain.isValidChain()));
---
+++ blockchain.ts
@@ -0,0 +1,98 @@
+const sha256 = require('crypto-js/sha256');
+
+type Txn = {
+  from: string,
+  to: string,
+};
+
+class Block {
+  hash: string;
+  constructor(
+    public txns: Txn,
+    public prevHash: string = '',
+  ) {
+    this.txns = txns;
+    this.prevHash = prevHash;
+    this.hash = this.computeHash();
+  }
+
+  computeHash(): string {
+    return sha256(this.prevHash + JSON.stringify(this.txns)).toString();
+  }
+}
+
+class BlockChain {
+  public blockchain: any;
+  public blockSize: number;
+  public state: any;
+  constructor() {
+    this.blockchain = [this.startGenesisBlock()];
+    this.blockSize = 2;
+    this.state = null;
+  }
+
+  startGenesisBlock() {
+    return new Block({} as Txn);
+  }
+
+  getLatestBlock() {
+    return this.blockchain[this.blockchain.length - 1];
+  }
+
+  isValidBlock(newBlock: Block) {
+    const { txns } = newBlock;
+    const txnKeys = Object.keys(txns);
+
+    if (txnKeys.length !== this.blockSize) {
+      return false;
+    }
+
+    const areCorrectKeys = txnKeys.map(key => ['from', 'to'].includes(key) ? true : false);
+    return areCorrectKeys.every(key => key === true);
+  }
+
+  addNewBlock(newBlock: Block) {
+    if (!this.isValidBlock(newBlock)) {
+      return;
+    }
+    newBlock.prevHash = this.getLatestBlock().hash; // Set its previous hash to the correct value
+    newBlock.hash = newBlock.computeHash(); // Recalculate its hash with this new prevHash value
+    this.blockchain.push(newBlock); // Add the block to our chain
+    this.state = newBlock.txns;
+  }
+
+  getBlock(hash: string): Block | undefined {
+    return this.blockchain.find(block => block.hash === hash);
+  }
+
+  getAccountBalance(account) {
+    return this.state.find(state => state.account === account);
+  }
+
+  isValidChain() { // Check to see that all the hashes are correct and the chain is therefore valid
+    for (let i = 1; i < this.blockchain.length; i++) { // Iterate through, starting after the genesis block
+      const currBlock = this.blockchain[i];
+      const prevBlock = this.blockchain[i -1];
+
+      // Is the hash correctly computed, or was it tampered with?
+      if (currBlock.hash !== currBlock.computeHash()) {
+        return false;
+      }
+
+      // Does it have the correct prevHash value?; ie: What a previous block tampered with?
+      if (currBlock.prevHash !== prevBlock.hash) {
+        return false;
+      }
+    }
+    return true; // If all the blocks are valid, return true
+  }
+}
+
+const a = new Block({from: "Joe", to: "Jane"})
+const b = new Block({from: "Jane", to: "Joe"})
+
+const chain = new BlockChain();
+chain.addNewBlock(a);
+chain.addNewBlock(b);
+console.log(chain);
+console.log(`is valid chain: ${chain.isValidChain()}`);
